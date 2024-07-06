3275011ea0088587b4cf354a5f865019dd2e8429
Justin Tran <justintran@Justins-MacBook-Pro-2.local>
2024-07-06 09:44:50 -0700

    finished draft

--- express.js
+++ express.js
@@ -1,3 +1,142 @@
+// const express = require('express');
+// const server = express();
+// const path = require('path');
+// const axios = require('axios');
+// const qs = require('qs');
+// require('dotenv').config();
+
+// server.use(express.json());
+// server.use(express.static(path.join(__dirname, 'public')));
+
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => console.log(`\n Running on port ${port}\n`));
+
+// const client_id = process.env.SPOTIFY_API_ID;
+// const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+// const auth_token = Buffer.from(`${client_id}:${client_secret}`, 'utf-8').toString('base64');
+
+// server.get('/profile/:username', async (req, res) => {
+//   const { username } = req.params;
+//   try {
+//     const profile = await getUserProfile(username);
+//     res.status(200).json(profile);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+// server.get('/playlists/:username', async (req, res) => {
+//   const { username } = req.params;
+//   try {
+//     const playlists = await getUserPlaylists(username);
+//     res.status(200).json(playlists);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+// server.get('/top-tracks/:username', async (req, res) => {
+//   const { username } = req.params;
+//   try {
+//     const topTracks = await getUserTopTracks(username);
+//     res.status(200).json(topTracks);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+// server.get('/top-artists/:username', async (req, res) => {
+//   const { username } = req.params;
+//   try {
+//     const topArtists = await getUserTopArtists(username);
+//     res.status(200).json(topArtists);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+// server.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// const getAuth = async () => {
+//   try {
+//     const token_url = 'https://accounts.spotify.com/api/token';
+//     const data = qs.stringify({ 'grant_type': 'client_credentials' });
+
+//     const response = await axios.post(token_url, data, {
+//       headers: {
+//         'Authorization': `Basic ${auth_token}`,
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//       }
+//     });
+//     return response.data.access_token;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// const getUserProfile = async (user_id) => {
+//   const access_token = await getAuth();
+//   const api_url = `https://api.spotify.com/v1/users/${user_id}`;
+//   try {
+//     const response = await axios.get(api_url, {
+//       headers: {
+//         'Authorization': `Bearer ${access_token}`
+//       }
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// const getUserPlaylists = async (user_id) => {
+//   const access_token = await getAuth();
+//   const api_url = `https://api.spotify.com/v1/users/${user_id}/playlists`;
+//   try {
+//     const response = await axios.get(api_url, {
+//       headers: {
+//         'Authorization': `Bearer ${access_token}`
+//       }
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// const getUserTopTracks = async (user_id) => {
+//   const access_token = await getAuth();
+//   const api_url = `https://api.spotify.com/v1/me/top/tracks`;
+//   try {
+//     const response = await axios.get(api_url, {
+//       headers: {
+//         'Authorization': `Bearer ${access_token}`
+//       }
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// const getUserTopArtists = async (user_id) => {
+//   const access_token = await getAuth();
+//   const api_url = `https://api.spotify.com/v1/me/top/artists`;
+//   try {
+//     const response = await axios.get(api_url, {
+//       headers: {
+//         'Authorization': `Bearer ${access_token}`
+//       }
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+
 const express = require('express');
 const server = express();
 const path = require('path');
@@ -13,30 +152,72 @@ server.listen(port, () => console.log(`\n Running on port ${port}\n`));
 
 const client_id = process.env.SPOTIFY_API_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
+
 const auth_token = Buffer.from(`${client_id}:${client_secret}`, 'utf-8').toString('base64');
 
-server.get('/profile/:username', async (req, res) => {
-  const { username } = req.params;
-  try {
-    const profile = await getUserProfile(username);
-    res.status(200).json(profile);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+// Spotify Scopes for the desired permissions
+const scopes = [
+  'user-read-private',
+  'user-read-email',
+  'user-read-recently-played',
+  'user-top-read',
+  'playlist-read-private',
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'user-follow-read',
+  'user-library-read',
+  'ugc-image-upload'
+].join(' ');
+
+const auth_query_parameters = qs.stringify({
+  response_type: 'code',
+  client_id: client_id,
+  scope: scopes,
+  redirect_uri: redirect_uri,
+  state: 'some_random_state'
 });
 
-server.get('/playlists/:username', async (req, res) => {
-  const { username } = req.params;
-  try {
-    const playlists = await getUserPlaylists(username);
-    res.status(200).json(playlists);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+server.get('/login', (req, res) => {
+  res.redirect(`https://accounts.spotify.com/authorize?${auth_query_parameters}`);
 });
 
-server.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+server.get('/callback', async (req, res) => {
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+
+  if (state === null) {
+    res.redirect('/#' + 
+      qs.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    const token_url = 'https://accounts.spotify.com/api/token';
+    const data = qs.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirect_uri
+    });
+
+    try {
+      const response = await axios.post(token_url, data, {
+        headers: {
+          'Authorization': `Basic ${auth_token}`,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+
+      const { access_token, refresh_token } = response.data;
+      res.redirect(`/#${qs.stringify({ access_token, refresh_token })}`);
+    } catch (error) {
+      console.error(error);
+      res.redirect('/#' + 
+        qs.stringify({
+          error: 'invalid_token'
+        }));
+    }
+  }
 });
 
 const getAuth = async () => {
@@ -56,9 +237,8 @@ const getAuth = async () => {
   }
 };
 
-const getUserProfile = async (user_id) => {
-  const access_token = await getAuth();
-  const api_url = `https://api.spotify.com/v1/users/${user_id}`;
+const getUserProfile = async (access_token) => {
+  const api_url = `https://api.spotify.com/v1/me`;
   try {
     const response = await axios.get(api_url, {
       headers: {
@@ -71,9 +251,8 @@ const getUserProfile = async (user_id) => {
   }
 };
 
-const getUserPlaylists = async (user_id) => {
-  const access_token = await getAuth();
-  const api_url = `https://api.spotify.com/v1/users/${user_id}/playlists`;
+const getUserPlaylists = async (access_token) => {
+  const api_url = `https://api.spotify.com/v1/me/playlists`;
   try {
     const response = await axios.get(api_url, {
       headers: {
@@ -86,110 +265,55 @@ const getUserPlaylists = async (user_id) => {
   }
 };
 
-// const express = require('express');
-// const server = express();
-// const path = require('path');
+// Add endpoints to fetch user data
+server.get('/profile', async (req, res) => {
+  const access_token = req.query.access_token;
+  try {
+    const profile = await getUserProfile(access_token);
+    res.status(200).json(profile);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
+server.get('/playlists', async (req, res) => {
+  const access_token = req.query.access_token;
+  try {
+    const playlists = await getUserPlaylists(access_token);
+    res.status(200).json(playlists);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-// server.use(express.json());
-// server.use(express.static(path.join(__dirname, 'public')));
+// Add additional endpoints for top tracks and top artists
+server.get('/top-tracks', async (req, res) => {
+  const access_token = req.query.access_token;
+  const api_url = `https://api.spotify.com/v1/me/top/tracks`;
+  try {
+    const response = await axios.get(api_url, {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    });
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-// // // define port variable
-// const port = process.env.PORT || 3000;
-// server.listen(port, ()=> console.log(`\n Running on port ${port}\n`))
-
-
-// const axios = require('axios');
-// const qs = require('qs');
-// require('dotenv').config();
-
-// const client_id = process.env.SPOTIFY_API_ID; // Your client id
-// const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
-// const auth_token = Buffer.from(`${client_id}:${client_secret}`, 'utf-8').toString('base64');
-// // let savedusername = jvintenct97;
-
-
-// //the first time savedusername gets assigned is when the user inputs a username into the search bar.
-// //after they press enter or search, savedusername is now reassigned (savedusername = whatever they typed)
-// //once enter is pressed or the search button and the textbox has text in it, thats the only time savedusername is ever reassigned.
-
-// // let savedusernamereassign = (textboxvalue) => {
-// //     if (!textboxvalue then reassign saved username){
-// //       savedusername = textobxvalue
-// //     }
-// // }
-
-// //from front end, when button or enter is clicked and textbox !== empty, then call savedusernamereassign.
-
-// server.get('/profile/:username', async (req, res) => {
-//   const { username } = req.params;
-//   try {
-//     const profile = await getUserProfile(username);
-//     res.status(200).json(profile);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// server.get('/', (req, res) => {
-//   return res.status(200).sendFile(path.resolve(__dirname, './index.html'));
-// })
-// const getAuth = async () => {
-//   try{
-//     //make post request to SPOTIFY API for access token, sending relavent info
-//     const token_url = 'https://accounts.spotify.com/api/token';
-//     const data = qs.stringify({'grant_type':'client_credentials'});
-
-//     const response = await axios.post(token_url, data, {
-//       headers: { 
-//         'Authorization': `Basic ${auth_token}`,
-//         'Content-Type': 'application/x-www-form-urlencoded' 
-//       }
-//     })
-//     //return access token
-//     console.log(response.data.access_token);
-//     return response.data.access_token;
-//     //console.log(response.data.access_token);   
-//   }catch(error){
-//     //on fail, log the error in console
-//     console.log(error);
-//   }
-// }
-
-// // const getAudioFeatures_Track = async (track_id) => {
-// //   //request token using getAuth() function
-// //   const access_token = await getAuth();
-// //   //console.log(access_token);
-
-// //   const api_url = `https://api.spotify.com/v1/audio-features/${track_id}`;
-// //   //console.log(api_url);
-// //   try{
-// //     const response = await axios.get(api_url, {
-// //       headers: {
-// //         'Authorization': `Bearer ${access_token}`
-// //       }
-// //     });
-// //     console.log(response.data);
-// //     return response.data;
-// //   }catch(error){
-// //     console.log(error);
-// //   }  
-// // };
-
-// const getUserProfile = async (user_id) => {
-//   const access_token = await getAuth();
-//   const api_url = `https://api.spotify.com/v1/users/${user_id}`
-//   try{
-//     const response = await axios.get(api_url, {
-//       headers: {
-//         'Authorization': `Bearer ${access_token}`
-//       }
-//     });
-//     console.log(response.data);
-//     return response.data;
-//   }catch(error){
-//     console.log(error);
-//   }  
-// }
-// console.log(getUserProfile('jvincentt97'));
+server.get('/top-artists', async (req, res) => {
+  const access_token = req.query.access_token;
+  const api_url = `https://api.spotify.com/v1/me/top/artists`;
+  try {
+    const response = await axios.get(api_url, {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    });
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
--- public/app.js
+++ public/app.js
@@ -1,25 +1,39 @@
+
 document.getElementById('fetchProfile').addEventListener('click', async () => {
-  const username = document.getElementById('username').value;
-  console.log('Fetch button clicked');
-  if (username) {
+  const params = new URLSearchParams(window.location.hash.substring(1));
+  const access_token = params.get('access_token');
+  
+  if (access_token) {
     try {
-      console.log('Fetching profile for:', username);
-      const profileResponse = await fetch(`/profile/${username}`);
+      console.log('Fetching profile');
+      const profileResponse = await fetch(`/profile?access_token=${access_token}`);
       const profile = await profileResponse.json();
       console.log('Profile fetched:', profile);
       displayProfile(profile);
 
-      console.log('Fetching playlists for:', username);
-      const playlistsResponse = await fetch(`/playlists/${username}`);
+      console.log('Fetching playlists');
+      const playlistsResponse = await fetch(`/playlists?access_token=${access_token}`);
       const playlists = await playlistsResponse.json();
       console.log('Playlists fetched:', playlists);
       displayPlaylists(playlists);
+
+      console.log('Fetching top tracks');
+      const topTracksResponse = await fetch(`/top-tracks?access_token=${access_token}`);
+      const topTracks = await topTracksResponse.json();
+      console.log('Top tracks fetched:', topTracks);
+      displayTopTracks(topTracks);
+
+      console.log('Fetching top artists');
+      const topArtistsResponse = await fetch(`/top-artists?access_token=${access_token}`);
+      const topArtists = await topArtistsResponse.json();
+      console.log('Top artists fetched:', topArtists);
+      displayTopArtists(topArtists);
     } catch (error) {
       console.error('Error fetching data:', error);
-      document.getElementById('profile').innerText = 'Error fetching profile or playlists';
+      document.getElementById('profile').innerText = 'Error fetching profile, playlists, top tracks, or top artists';
     }
   } else {
-    alert('Please enter a Spotify username');
+    window.location = '/login';  // Redirect to login if no access token
   }
 });
 
@@ -37,6 +51,8 @@ function displayPlaylists(playlists) {
   const playlistsDiv = document.getElementById('playlists');
   playlistsDiv.innerHTML = '<h3>Public Playlists:</h3>';
   playlistsDiv.style.display = 'flex'; // Ensure playlists container is flex
+  playlistsDiv.style.flexDirection = 'column';
+  playlistsDiv.style.alignItems = 'center';
 
   if (playlists.items.length === 0) {
     playlistsDiv.innerHTML += '<p>No public playlists found</p>';
@@ -47,6 +63,50 @@ function displayPlaylists(playlists) {
           <img src="${playlist.images[0]?.url}" alt="Playlist Image">
           <p><a href="${playlist.external_urls.spotify}" target="_blank">${playlist.name}</a></p>
           <p>${playlist.tracks.total} tracks</p>
+        </div>
+      `;
+    });
+  }
+}
+
+function displayTopTracks(topTracks) {
+  const topTracksDiv = document.getElementById('topTracks');
+  topTracksDiv.innerHTML = '<h3>Top Tracks:</h3>';
+  topTracksDiv.style.display = 'flex'; // Ensure top tracks container is flex
+  topTracksDiv.style.flexDirection = 'column';
+  topTracksDiv.style.alignItems = 'center';
+
+  if (topTracks.items.length === 0) {
+    topTracksDiv.innerHTML += '<p>No top tracks found</p>';
+  } else {
+    topTracks.items.forEach(track => {
+      topTracksDiv.innerHTML += `
+        <div class="track">
+          <img src="${track.album.images[0]?.url}" alt="Track Image">
+          <p><a href="${track.external_urls.spotify}" target="_blank">${track.name}</a> by ${track.artists.map(artist => artist.name).join(', ')}</p>
+          <p>Album: ${track.album.name}</p>
+        </div>
+      `;
+    });
+  }
+}
+
+function displayTopArtists(topArtists) {
+  const topArtistsDiv = document.getElementById('topArtists');
+  topArtistsDiv.innerHTML = '<h3>Top Artists:</h3>';
+  topArtistsDiv.style.display = 'flex'; // Ensure top artists container is flex
+  topArtistsDiv.style.flexDirection = 'column';
+  topArtistsDiv.style.alignItems = 'center';
+
+  if (topArtists.items.length === 0) {
+    topArtistsDiv.innerHTML += '<p>No top artists found</p>';
+  } else {
+    topArtists.items.forEach(artist => {
+      topArtistsDiv.innerHTML += `
+        <div class="artist">
+          <img src="${artist.images[0]?.url}" alt="Artist Image">
+          <p><a href="${artist.external_urls.spotify}" target="_blank">${artist.name}</a></p>
+          <p>Followers: ${artist.followers.total}</p>
         </div>
       `;
     });
